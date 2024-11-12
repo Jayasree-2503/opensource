54734b515b2a09d99b8309fb8d4ec21b5a6c8d62
Jayasree-2503 <jmogusani@gmail.com>
2024-11-12 10:38:13 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Solution
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      double X = sc.nextInt();
+      double P = X / 0.95;
+      System.out.println("%.4f\n", P);
+    }
+  }
